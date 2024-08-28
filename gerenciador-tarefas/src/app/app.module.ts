c5d8051ec599dd5bbe38dafcc1a893a15774acd2
Douglas <dev.douggo@gmail.com>
2024-08-28 19:28:50 -0300

    Adds RouterModule

---
+++ gerenciador-tarefas/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
