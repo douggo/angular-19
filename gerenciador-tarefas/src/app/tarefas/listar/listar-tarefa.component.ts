3d637d5059f2b46e3e4aceddd6c914ebed0f8834
Douglas <dev.douggo@gmail.com>
2024-08-29 21:20:56 -0300

    Finished development for listing tarefas

--- gerenciador-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
+++ gerenciador-tarefas/src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tarefa, TarefaService } from '../shared';
 
 @Component({
   selector: 'app-listar-tarefa',
   templateUrl: './listar-tarefa.component.html',
   styleUrl: './listar-tarefa.component.css'
 })
-export class ListarTarefaComponent {
+export class ListarTarefaComponent implements OnInit {
+  
+  public tarefas: Tarefa[];
+
+  constructor(private tarefaService: TarefaService) {}
+
+  public ngOnInit(): void {
+    //this.tarefas = this.listarTodos();
+    this.tarefas = [
+      new Tarefa(1, "Tarefa 1", false),
+      new Tarefa(2, "Tarefa 2", true)
+    ]
+  }
+
+  public listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
+  }
+
+  public remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm('Are you sure? The tarefa '.concat(tarefa.nome).concat(' will be removed!'))) {
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
+
 
 }
--- gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ListarTarefaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     TarefaService
