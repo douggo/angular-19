0e5cb6e67bf6149f5343b31ae6d32cd139e8e498
Douglas <dev.douggo@gmail.com>
2024-08-29 21:25:22 -0300

    Creates files for tarefa creation

--- gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
@@ -4,10 +4,12 @@ import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CadastrarTarefaComponent } from './cadastrar';
 
 @NgModule({
   declarations: [
-    ListarTarefaComponent
+    ListarTarefaComponent,
+    CadastrarTarefaComponent
   ],
   imports: [
     CommonModule,
