5d492685809a0575800bccaae64b93cc8a43f58d
Douglas <dev.douggo@gmail.com>
2024-08-31 18:33:49 -0300

    Finished development

--- jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JogoDaVelhaService } from './shared';
 
 @Component({
   selector: 'app-jogo-da-velha',
   templateUrl: './jogo-da-velha.component.html',
   styleUrl: './jogo-da-velha.component.css'
 })
-export class JogoDaVelhaComponent {
+export class JogoDaVelhaComponent implements OnInit {
+
+  constructor(private jogoDaVelhaService: JogoDaVelhaService) {}
+
+  ngOnInit(): void {
+    this.jogoDaVelhaService.inicializar()
+  }
+
+  public get showInicio(): boolean {
+    return this.jogoDaVelhaService.showInicio;
+  }
+
+  public get showTabuleiro(): boolean {
+    return this.jogoDaVelhaService.showTabuleiro;
+  }
+
+  public get showFinal(): boolean {
+    return this.jogoDaVelhaService.showFinal;
+  }
+
+  public iniciarJogo(): void {
+    this.jogoDaVelhaService.iniciarJogo();
+  }
+
+  public jogar(posX: number, posY: number): void {
+    this.jogoDaVelhaService.jogar(posX, posY);
+  }
+
+  public exibirX(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirX(posX, posY);
+  }
+
+  public exibirO(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirO(posX, posY);
+  }
+
+  public exibirVitoria(posX: number, posY: number): boolean {
+    return this.jogoDaVelhaService.exibirVitoria(posX, posY);
+  }
+
+  public get jogador(): number {
+    return this.jogoDaVelhaService.jogador;
+  }
+
+  public novoJogo(): void {
+    this.jogoDaVelhaService.novoJogo();
+  }
 
 }
--- jogo-da-velha/src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
+++ jogo-da-velha/src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
@@ -8,7 +8,7 @@ export class JogoDaVelhaService {
   private readonly TAM_TAB: number = 3;
   private readonly X: number = 1;
   private readonly O: number = 2;
-  private readonly EMPTY: number = 0;
+  private readonly VAZIO: number = 0;
 
   private tabuleiro: any;
   private numMovimentos: number;
@@ -19,107 +19,125 @@ export class JogoDaVelhaService {
   private _showTabuleiro: boolean;
   private _showFinal: boolean;
 
-  constructor() {}
+  constructor() { }
 
-  public inicializar(): void {
+  inicializar(): void {
     this._showInicio = true;
     this._showTabuleiro = false;
     this._showFinal = false;
-
     this.numMovimentos = 0;
     this._jogador = this.X;
     this.vitoria = false;
-
     this.inicializarTabuleiro();
   }
 
-  private inicializarTabuleiro(): void {
+  inicializarTabuleiro(): void {
     this.tabuleiro = [this.TAM_TAB];
     for (let i = 0; i < this.TAM_TAB; i++) {
-      this.tabuleiro[i] = [this.EMPTY, this.EMPTY, this.EMPTY]
+      this.tabuleiro[i] = [this.VAZIO, this.VAZIO, this.VAZIO];
     }
   }
 
-  public get showInicio(): boolean {
+  get showInicio(): boolean {
     return this._showInicio;
   }
 
-  public get showTabuleiro(): boolean {
+  get showTabuleiro(): boolean {
     return this._showTabuleiro;
   }
 
-  public get showFinal(): boolean {
+  get showFinal(): boolean {
     return this._showFinal;
   }
 
-  public get jogador(): number {
+  get jogador(): number {
     return this._jogador;
   }
 
-  public iniciarJogo(): void {
+  iniciarJogo(): void {
     this._showInicio = false;
     this._showTabuleiro = true;
   }
 
-  public jogar(posX: number, posY: number): void {
-    if (this.tabuleiro[posX][posY] !== this.EMPTY || this.vitoria) {
+  jogar(posX: number, posY: number): void {
+    // jogada inválida
+    if (this.tabuleiro[posX][posY] !== this.VAZIO || 
+      this.vitoria) {
       return;
     }
 
     this.tabuleiro[posX][posY] = this._jogador;
     this.numMovimentos++;
-    this.vitoria = this.fimJogo(posX, posY, this.tabuleiro, this._jogador);
+    this.vitoria = this.fimJogo(posX, posY, 
+      this.tabuleiro, this._jogador);
+    this._jogador = (this._jogador === this.X) ? this.O : this.X;
 
     if (!this.vitoria && this.numMovimentos < 9) {
       this.cpuJogar();
     }
 
-    if (this.vitoria) {
+    // houve vitória
+    if (this.vitoria !== false) {
       this._showFinal = true;
     }
 
+    // empate
     if (!this.vitoria && this.numMovimentos === 9) {
       this._jogador = 0;
       this._showFinal = true;
     }
   }
 
+  fimJogo(linha: number, coluna: number, 
+      tabuleiro: any, jogador: number) {
+    let fim: any = false;
 
-  private fimJogo(linha: number, coluna: number, tabuleiro: any, jogador: number): false|number[][] {
-    let fim: false|number[][] = false;
+    // valida a linha
+    if (tabuleiro[linha][0] === jogador && 
+      tabuleiro[linha][1] === jogador && 
+      tabuleiro[linha][2] === jogador) {
+      fim = [[linha, 0], [linha, 1], [linha, 2]];
+    }
 
-    if (tabuleiro[0][coluna] === jogador && tabuleiro[1][coluna] === jogador && tabuleiro[2][coluna] === jogador) {
+    // valida a coluna
+    if (tabuleiro[0][coluna] === jogador && 
+      tabuleiro[1][coluna] === jogador && 
+      tabuleiro[2][coluna] === jogador) {
       fim = [[0, coluna], [1, coluna], [2, coluna]];
     }
 
-    if (tabuleiro[linha][0] === jogador && tabuleiro[linha][0] === jogador && tabuleiro[linha][2] === jogador) {
-      fim = [[linha, 0], [linha, 1], [linha, 3]];
+    // valida as diagonais
+    if (tabuleiro[0][0] === jogador && 
+      tabuleiro[1][1] === jogador && 
+      tabuleiro[2][2] === jogador) {
+      fim = [[0, 0], [1, 1], [2, 2]];
     }
 
-    if (tabuleiro[0][0] === jogador && tabuleiro[1][1] === jogador && tabuleiro[2][2] == jogador) {
-      fim = [[0,0], [1,1], [2,2]];
-    }
-
-    if (tabuleiro[0][2] === jogador && tabuleiro[1][1] === jogador && tabuleiro[2][0] == jogador) {
-      fim = [[0,2], [1,1], [2,0]];
+    if (tabuleiro[0][2] === jogador && 
+      tabuleiro[1][1] === jogador && 
+      tabuleiro[2][0] === jogador) {
+      fim = [[0, 2], [1, 1], [2, 0]];
     }
 
     return fim;
   }
 
-  private cpuJogar(): void {
+  cpuJogar(): void {
+    // verifica jogada de vitória
     let jogada: number[] = this.obterJogada(this.O);
 
     if (jogada.length <= 0) {
+      // tenta jogar para evitar derrota
       jogada = this.obterJogada(this.X);
     }
 
     if (jogada.length <= 0) {
+      // joga aleatório
       let jogadas: any = [];
-      for (let i = 0; i < this.TAM_TAB; i++) {
-        for (let j = 0; j < this.TAM_TAB; j++) {
-          if (this.tabuleiro[i][j] === this.EMPTY) {
-            jogadas.push([i,j]);
+      for (let i=0; i<this.TAM_TAB; i++) {
+        for (let j=0; j<this.TAM_TAB; j++) {
+          if (this.tabuleiro[i][j] === this.VAZIO) {
+            jogadas.push([i, j]);
           }
         }
       }
@@ -129,36 +147,37 @@ export class JogoDaVelhaService {
 
     this.tabuleiro[jogada[0]][jogada[1]] = this._jogador;
     this.numMovimentos++;
-    this.vitoria = this.fimJogo(jogada[0], jogada[1], this.tabuleiro, this._jogador);
+    this.vitoria = this.fimJogo(jogada[0], jogada[1],
+        this.tabuleiro, this._jogador);
     this._jogador = (this._jogador === this.X) ? this.O : this.X;
   }
 
-  public obterJogada(jogador: number): number[] {
+  obterJogada(jogador: number): number[] {
     let tab = this.tabuleiro;
     for (let lin = 0; lin < this.TAM_TAB; lin++) {
-      for (let col = 0; col < this.TAM_TAB; lin++) {
-        if (tab[lin][col] !== this.EMPTY) {
+      for (let col = 0; col < this.TAM_TAB; col++) {
+        if (tab[lin][col] !== this.VAZIO) {
           continue;
         }
         tab[lin][col] = jogador;
         if (this.fimJogo(lin, col, tab, jogador)) {
           return [lin, col];
         }
-        tab[lin][col] == this.EMPTY
+        tab[lin][col] = this.VAZIO;
       }
     }
     return [];
   }
 
-  public exibirX(posX: number, posY: number): boolean {
+  exibirX(posX: number, posY: number): boolean {
     return this.tabuleiro[posX][posY] === this.X;
   }
 
-  public exibirO(posX: number, posY: number): boolean {
+  exibirO(posX: number, posY: number): boolean {
     return this.tabuleiro[posX][posY] === this.O;
   }
-
-  public exibirVitoria(posX: number, posY: number): boolean {
+  
+  exibirVitoria(posX: number, posY: number): boolean {
     let exibirVitoria: boolean = false;
 
     if (!this.vitoria) {
@@ -175,7 +194,7 @@ export class JogoDaVelhaService {
     return exibirVitoria;
   }
 
-  public novoJogo(): void {
+  novoJogo(): void {
     this.inicializar();
     this._showFinal = false;
     this._showInicio = false;
